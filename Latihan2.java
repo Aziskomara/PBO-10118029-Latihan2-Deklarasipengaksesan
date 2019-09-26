36fbb90a9b377e29eff8a259f06fee2daa8aa3a2
Aziskomara <55784203+Aziskomara@users.noreply.github.com>
2019-09-26 15:20:45 +0700

    Add files via upload

---
+++ Latihan2.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan2;
+
+/**
+ * Nama     : Azis komara
+ * Kelas    : IF-1
+ * Nim      : 10118029
+ * deskiripsi program   : Deklalari pengaksesan
+ * 
+ * @author Azis komara
+ */
+public class Latihan2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //deklarasi variabel
+        int nilaiInt;
+        final double PHI = 3.14; //konstanta uppercas
+        boolean nilaiLogika;
+        char nilaiKarakter;
+        
+        // memberi niali ke variabel
+        nilaiInt = 78;
+        nilaiLogika =false;
+        nilaiKarakter ='D';
+        
+        //
+        System.out.println();
+        System.out.println("Isi variabel nilai = " + nilaiInt);
+        System.out.println("Isi nilai variabel PHI = " + PHI);
+        System.out.println("Isi variabel logika = " + nilaiLogika);
+        System.out.println("Isi variabel Karakter = " + nilaiKarakter);
+    }
+    
+}
